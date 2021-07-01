44d6645fdba0a9575f05bb4b83786feaafca409e
Elcris1 <60231775+Elcris1@users.noreply.github.com>
2021-07-01 10:48:52 +0200

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SwagTutorial',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
